387ffc35cd0aa958df84ae281602ee48ba46e35c
Douglas Costa <douglassilvacosta06@gmail.com>
2025-01-26 21:59:16 -0300

    adicionado metodo put e post

--- main.py
+++ main.py
@@ -33,5 +33,21 @@ def get_book_by_id(id):
             return jsonify(book)
     return None
 
+# editar um livro por ID
+@app.route("/books/<int:id>", methods=["PUT"])
+def update_book_by_id(id):
+    updated_book = request.get_json()
+    for index, book in enumerate(books):
+        if book["id"] == id:
+            books[index].update(updated_book)
+            return jsonify(book[index])
+    return None
+
+# Adicionar um livro
+@app.route("/books", methods=["POST"])
+def add_book():
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify(books)
 
 app.run(port=5000, host="localhost", debug=True)
