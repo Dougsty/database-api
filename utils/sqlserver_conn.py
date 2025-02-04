504c4acc41a4d8a43c4ae461be8bd425ec87f974
Douglas Costa <douglassilvacosta06@gmail.com>
2025-02-03 22:15:13 -0300

    adicionado conexão com o servidor sqlserver

---
+++ utils/sqlserver_conn.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+import os
+
+def sqlconnection() -> create_engine:
+    """
+    Establishes a connection to a SQL Server database using the provided credentials.
+
+    Returns:
+        engine (sqlalchemy.engine.Engine): The SQLAlchemy engine object representing the connection to the database.
+    """
+
+    # Login info
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("DB_PASSWORD")
+
+    # Server info
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_NAME")
+    port = os.getenv("DB_PORT")
+    # Construct the connection string
+    connection_string = f"mssql+pymssql://{username}:{password}@{server},{port}/{database}"
+
+    try:
+        # Create the SQLAlchemy engine
+        engine = create_engine(connection_string)
+
+        # Test the connection
+        with engine.connect() as connection:
+            print("Conexão bem-sucedida!")
+        return engine
+    except SQLAlchemyError as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        raise
